ff8a9270f3e2a90a250d988f46a31cf97a238520
tienanh1409.work <tienanh1409.work@gmail.com>
2024-03-15 14:27:59 +0700

    add commit hash to image

--- src/main/java/com/example/reactive/repository/UserRepository.java
+++ src/main/java/com/example/reactive/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.example.reactive.entity.User;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Flux;
+
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
     @Query("{'name': ?0 }")
     Flux<User> findByName(final String name);
